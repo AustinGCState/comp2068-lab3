dad47ffda35cbf6ba5ef924996e6e90fd5f182d8
Austin <austinmkpc@hotmail.com>
2016-02-04 18:39:57 -0500

    added contact

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
 module.exports = router;
