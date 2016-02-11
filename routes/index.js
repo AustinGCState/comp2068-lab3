fc2d9e18e5f499227e750fe6b79e7985472b8706
Austin <austinmkpc@hotmail.com>
2016-02-10 23:11:39 -0500

    Content and CSS

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('index', { title: "Austin Cameron's Portfolio" });
 });
 
+//About Page
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
+//Contact Page
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
+});
+//Projects Page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+//Services Page
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 module.exports = router;
